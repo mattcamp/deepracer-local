a105c429c5b0384e39e56fe62c0c1ac546e5b56d
adam-aph <adam.p.hajduk@gmail.com>
2020-03-17 01:21:25 +0100

    ROS Video mapping, sagemaker logs, upload script

--- start-training.sh
+++ start-training.sh
@@ -9,3 +9,16 @@ export ROBOMAKER_COMMAND="./run.sh run distributed_training.launch"
 export CURRENT_UID=$(id -u):$(id -g)
 
 docker-compose -f ./docker-compose.yml up -d
+
+#sleep for 20 seconds to allow the containers to start
+sleep 20
+
+echo 'Attempting to pull up sagemaker logs...'
+SAGEM_ID="$(docker ps | awk ' /sagemaker/ { print $1 }')"
+gnome-terminal -x sh -c "docker logs -f $SAGEM_ID"
+nohup docker logs -f $SAGEM_ID >data/robomaker/log/sagemaker_$(date +"%Y%m%d%H%M").log &
+##gnome-terminal -x sh -c "docker logs -f $(docker ps | awk ' /sagemaker/ { print $1 }')"
+
+echo 'Attempting to open the viewer...'
+gnome-terminal -x sh -c "echo viewer;x-www-browser -new-window http://localhost:8888/stream_viewer?topic=/racecar/deepracer/kvs_stream;sleep 1;wmctrl -r kvs_stream -b remove,maximized_vert,maximized_horz;sleep 1;wmctrl -r kvs_stream -e 1,100,100,720,640"
+
--- stop-training.sh
+++ stop-training.sh
@@ -4,3 +4,5 @@ docker-compose -f ./docker-compose.yml down
 
 docker stop $(docker ps | awk ' /sagemaker/ { print $1 }')
 docker rm $(docker ps -a | awk ' /sagemaker/ { print $1 }')
+
+wmctrl -c kvs_stream
