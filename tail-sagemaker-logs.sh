24b22443200333ac95e4b1dbbb60334c115820b1
matt <matt@jigsaw.xyz>
2020-04-02 14:10:49 +0100

    Add startup tmux scripts

---
+++ tail-sagemaker-logs.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+while [ -z $SAGEMAKER_ID ]
+do
+  echo "Waiting for sagemaker to start"
+  SAGEMAKER_ID=$(docker ps | grep sagemaker | awk '{ print $1}')
+  sleep 1
+done
+
+docker logs -f $SAGEMAKER_ID
---
+++ tmux-logs.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+TMUX=$(which tmux)
+
+if [ ! -z $TMUX ]
+then
+  echo "tmux not found in path"
+  exit 0
+fi
+
+tmux new-session -d bash
+tmux split-window -h bash
+#sends keys to first and second terminals
+tmux send -t 0:0.0 "docker logs -f robomaker" C-m
+tmux send -t 0:0.1 "./tail-sagemaker-logs.sh" C-m
+tmux -2 attach-session -d
+
