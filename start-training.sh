4db83d0c6f82c1b01d75e5d456b50ffd6deee7bf
Maciej-GBS <12584491+Maciej-GBS@users.noreply.github.com>
2020-06-02 22:17:12 +0200

    Increased wait time to 30s

--- start-training.sh
+++ start-training.sh
@@ -13,14 +13,14 @@ export CURRENT_UID=$(id -u):$(id -g)
 docker-compose -f ./docker-compose.yml up -d
 
 if [ "$ENABLE_LOCAL_DESKTOP" = true ] ; then
-    echo "Starting desktop mode... waiting 20s for Sagemaker container to start"
-    sleep 20
+    echo "Starting desktop mode... waiting 30s for Sagemaker container to start"
+    sleep 30
 
     echo 'Attempting to pull up sagemaker logs...'
     SAGEMAKER_ID="$(docker ps | awk ' /sagemaker/ { print $1 }')"
 
     echo 'Attempting to open stream viewer and logs...'
-    gnome-terminal -- sh -c "echo viewer;x-www-browser -new-window http://localhost:8888/stream_viewer?topic=/racecar/deepracer/kvs_stream;sleep 1;wmctrl -r kvs_stream -b remove,maximized_vert,maximized_horz;sleep 1;wmctrl -r kvs_stream -e 1,100,100,720,640"
+    gnome-terminal --tab -- sh -c "echo viewer;x-www-browser -new-window http://localhost:8888/stream_viewer?topic=/racecar/deepracer/kvs_stream;sleep 1;wmctrl -r kvs_stream -b remove,maximized_vert,maximized_horz;sleep 1;wmctrl -r kvs_stream -e 1,100,100,720,640"
     gnome-terminal --tab -- sh -c "docker logs -f $SAGEMAKER_ID"
 else
     echo "Started in headless server mode. Set ENABLE_LOCAL_DESKTOP to true in config.env for desktop mode."
